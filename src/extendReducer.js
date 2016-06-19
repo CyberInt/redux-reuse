cf301424788711a64b2a6948631f00446e057a02
xamfoo <xamfoo@all.repo>
2016-06-19 11:49:52 -0400

    Add extendReducer and main npm file

---
+++ src/extendReducer.js
@@ -0,0 +1,15 @@
+/**
+ * Extends an existing reducer with additional handlers
+ * @param {function} reducer - a reducer function
+ * @param {object} handlers - handler object like used in createReducer()
+ * @returns {function} returns a reducer
+ */
+const extendReducer = (reducer, handlers) => (state, action) => {
+  const stateForReducer = handlers.hasOwnProperty(action.type)
+    ? handlers[action.type](state, action)
+    : state;
+
+  return reducer(stateForReducer, action);
+};
+
+export default extendReducer;
