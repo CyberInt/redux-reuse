c8f085b637a0e60db57e0eaa5e8ee0baf0297e50
xamfoo <xamfoo@all.repo>
2016-06-28 04:19:55 -0400

    Build /dist

---
+++ dist/reduxReuse.js
@@ -0,0 +1,113 @@
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define([], factory);
+	else if(typeof exports === 'object')
+		exports["reduxReuse"] = factory();
+	else
+		root["reduxReuse"] = factory();
+})(this, function() {
+return /******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+
+
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+
+	var _extendReducer = __webpack_require__(1);
+
+	Object.defineProperty(exports, 'extendReducer', {
+	  enumerable: true,
+	  get: function get() {
+	    return _interopRequireDefault(_extendReducer).default;
+	  }
+	});
+
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+/***/ },
+/* 1 */
+/***/ function(module, exports) {
+
+	"use strict";
+
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+	/**
+	 * Extends an existing reducer with additional handlers
+	 * @param {function} reducer - a reducer function
+	 * @param {object} handlers - handler object like used in createReducer()
+	 * @returns {function} returns a reducer
+	 */
+	var extendReducer = function extendReducer(reducer, handlers) {
+	  return function (state, action) {
+	    var stateForReducer = handlers.hasOwnProperty(action.type) ? handlers[action.type](state, action) : state;
+
+	    return reducer(stateForReducer, action);
+	  };
+	};
+
+	exports.default = extendReducer;
+
+/***/ }
+/******/ ])
+});
+;ction.type) ? handlers[action.type](state, action) : state;
+
+	    return reducer(stateForReducer, action);
+	  };
+	};
+
+	exports.default = extendReducer;
+
+/***/ }
+/******/ ])
+});
+;
