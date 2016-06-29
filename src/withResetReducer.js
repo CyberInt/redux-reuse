1e63d42a16b0bd87b615c9bb03ab00320a226f64
xamfoo <xamfoo@all.repo>
2016-06-29 09:27:02 -0400

    Update withResetReducer

--- src/withResetReducer.js
+++ src/withResetReducer.js
@@ -1,11 +1,27 @@
 import { extendReducer } from './extendReducer';
+import { nullReducer } from './nullReducer';
 
-export const RESET_STATE = 'RESET_STATE';
+/**
+ * These are private action types reserved by redux-reuse.
+ *
+ * From redux:
+ * For any unknown actions, you must return the current state.
+ * If the current state is undefined, you must return the initial state.
+ * Do not reference these action types directly in your code.
+ */
+export const actionTypes = {
+  RESET: '@@reduxReuse/RESET'
+};
 
-export const withResetReducer = (...actionTypes) => (reducer) => extendReducer(
-  reducer,
-  actionTypes.reduce((handlers, actionType) => ({
-    ...handlers,
-    [actionType]: () => reducer(undefined, { type: RESET_STATE }),
-  }), {})
-);
+/**
+ * Creates a reducer wrapper which resets the state for the given action type(s)
+ * @param {...string} actionTypes
+ * @returns {function} a function of signature (reducer) => newReducer
+ */
+export const withResetReducer = (...actionTypes) => (reducer = nullReducer) =>
+  extendReducer(reducer,
+    actionTypes.reduce((handlers, actionType) => ({
+      ...handlers,
+      [actionType]: () => reducer(undefined, { type: actionTypes.RESET }),
+    }), {})
+  );
