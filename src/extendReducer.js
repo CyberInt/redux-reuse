345b2205eb155c6c76e7d1ef3759689f0b2bea6a
xamfoo <xamfoo@all.repo>
2016-06-29 07:45:10 -0400

    Improve jsdoc of extendReducer()

--- src/extendReducer.js
+++ src/extendReducer.js
@@ -1,7 +1,11 @@
 /**
- * Extends an existing reducer with additional handlers
+ * Extends an existing reducer with additional action type handlers
  * @param {function} reducer - a reducer function
- * @param {object} handlers - handler object like used in createReducer()
+ * @param {object} handlers
+ *   handler object
+ *     where key is the action type to be handled
+ *           value is a reducer function with signature:
+ *             (state, action) => newState
  * @returns {function} returns a reducer
  */
 export const extendReducer = (reducer, handlers) => (state, action) => {
