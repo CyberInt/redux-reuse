aa1b7cc0a81b2498575e956da3b7321c97c8b055
xamfoo <xamfoo@all.repo>
2016-06-29 09:27:25 -0400

    Update exports

--- src/reduxReuse.js
+++ src/reduxReuse.js
@@ -1,1 +1,8 @@
-export { extendReducer as extendReducer } from './extendReducer';
+export { extendReducer } from './extendReducer';
+export { initialReducer } from './initialReducer';
+export { nullReducer } from './nullReducer';
+
+export { withActionFilterReducer } from './withActionFilterReducer';
+export { withMapStateReducer } from './withMapStateReducer';
+export { withPayloadReducer } from './withPayloadReducer';
+export { withResetReducer } from './withResetReducer';
